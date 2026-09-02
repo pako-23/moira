79d54cce7045ef3b76412df1b3d800f335ef4c6d
pako-23 <pasqualepolverino98@gmail.com>
2026-09-02 09:50:53 +0200

    delete more unused classes from mora.util.service tests

--- util/src/test/java/moira/util/service/ExampleTest.java
+++
@@ -1,6 +0,0 @@
-package moira.util.service;
-
-public class ExampleTest {
-  @org.junit.Test
-  public void testExample() {}
-}
--- util/src/test/java/moira/util/service/SecondExampleTest.java
+++
@@ -1,9 +0,0 @@
-package moira.util.service;
-
-public class SecondExampleTest {
-  @org.junit.Test
-  public void testSomething() {}
-
-  @org.junit.Test
-  public void testSomethingElse() {}
-}
